ef8fb014c7e8077266555b3e743f93690366e288
CleanLake <three_star123@hotmail.com>
2020-07-25 21:41:07 +0900

    固定ページ追加(association)、メニューのリンク先の更新

---
+++ 404.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta name="robots" content="noindex,follow" />              
+        <meta charset="utf-8">
+        <title>公益財団法人 アジア学生文化協会</title>
+        <meta name="description" content="日本とアジア諸国の青年学生が共同生活を通じて、人間的和合と学術、文化および経済の交流をはかることにより、アジアの親善と世界の平和に貢献することを目的とする財団法人。">
+   		<meta name="viewport" content="width=device-width,user-scalable=no,shrink-to-fit=yes">			
+		<script src="<?php echo get_template_directory_uri(); ?>/js/viewport.js"></script>
+
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
+    </head>
+    <body>
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+get_header();
+?>
+		<section class="info_vox sec_pad_1" style="margin-top: 100px;">
+			<h2 class="h_type_1">お探しのページが見つかりません。</h2>
+			<span>404 NOT FOUND</span>							
+			<article class="clearfix vox_type_2">
+				<div class="tx">
+					<div class="inn" style="text-align:center;">
+						<span class="in_tx"><br />メニューからご覧になりたいページをご選択ください。
+						</span>
+					</div>
+				</div>
+			</article>
+		</section>
+<?php
+get_footer();
+?>
+    </body>
+</html>
