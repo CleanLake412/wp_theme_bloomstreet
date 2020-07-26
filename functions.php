1416722b75b8af745970581bde861017bfc568d8
CleanLake <three_star123@hotmail.com>
2020-07-26 18:54:52 +0900

    WP管理画面にメニュー設定が表示されるように

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+    add_theme_support( 'menus' );
+
+    function wpb_custom_new_menu() {
+        register_nav_menu('header-menu', 'Header Menu');
+        register_nav_menu('footer-menu', 'Footer Menu');
+    }
+    add_action( 'init', 'wpb_custom_new_menu' );
